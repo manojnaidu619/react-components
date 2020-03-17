d0a09e46247e72510a2e0652ea62e89785228c95
Manoj Naidu <manojnaidu619@gmail.com>
2020-03-17 15:39:36 +0530

    Added props children

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import ApprovalCard from './ApprovalCard'
 const App = () => {
    return (
     <div class="ui container comments">
-        <ApprovalCard/>
-        <CommentDetail author="Sam" avatar={faker.image.avatar()} message="Cool man!"/>
+        <ApprovalCard><CommentDetail author="Sam" avatar={faker.image.avatar()} message="Cool man!"/></ApprovalCard>
         <CommentDetail author="Arthur" avatar={faker.image.avatar()} message="I like this"/>
         <CommentDetail author="Daniel" avatar={faker.image.avatar()} message="Amazing Post..."/>
     </div>
