5bef9a7cdbec834cb4c8a068d8c51821cc6296e3
Manoj Naidu <manojnaidu619@gmail.com>
2020-03-17 13:17:55 +0530

    Duplicated single component

--- src/index.js
+++ src/index.js
@@ -5,25 +5,63 @@ import faker from 'faker'
 const App = () => {
    return (
     <div class="ui comments">
-    <div class="comment">
-      <a class="avatar">
-        <img src={faker.image.avatar()}/>
-      </a>
-      <div class="content">
-        <a class="author">Stevie Feliciano</a>
-        <div class="metadata">
-          <div class="date">2 days ago</div>
-          <div class="rating">
-            <i class="star icon"></i>
-            5 Faves
-          </div>
+        <div class="comment">
+        <a class="avatar">
+            <img src={faker.image.avatar()}/>
+        </a>
+            <div class="content">
+                <a class="author">Stevie Feliciano</a>
+                <div class="metadata">
+                <div class="date">2 days ago</div>
+                <div class="rating">
+                    <i class="star icon"></i>
+                    5 Faves
+                </div>
+                </div>
+                <div class="text">
+                Hey guys, I hope this example comment is helping you read this documentation.
+                </div>
+            </div>
         </div>
-        <div class="text">
-          Hey guys, I hope this example comment is helping you read this documentation.
+
+        <div class="comment">
+        <a class="avatar">
+            <img src={faker.image.avatar()}/>
+        </a>
+            <div class="content">
+                <a class="author">Stevie Feliciano</a>
+                <div class="metadata">
+                <div class="date">2 days ago</div>
+                <div class="rating">
+                    <i class="star icon"></i>
+                    5 Faves
+                </div>
+                </div>
+                <div class="text">
+                Hey guys, I hope this example comment is helping you read this documentation.
+                </div>
+            </div>
         </div>
-      </div>
+
+        <div class="comment">
+        <a class="avatar">
+            <img src={faker.image.avatar()}/>
+        </a>
+            <div class="content">
+                <a class="author">Stevie Feliciano</a>
+                <div class="metadata">
+                <div class="date">2 days ago</div>
+                <div class="rating">
+                    <i class="star icon"></i>
+                    5 Faves
+                </div>
+                </div>
+                <div class="text">
+                Hey guys, I hope this example comment is helping you read this documentation.
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
    )
 }
 
