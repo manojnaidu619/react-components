f6a9810d0098c887ed8623c0b1fc73fcfeffcc5f
Manoj Naidu <manojnaidu619@gmail.com>
2020-03-17 13:29:27 +0530

    Serving comment component

--- src/index.js
+++ src/index.js
@@ -1,47 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import faker from 'faker'
+import CommentDetail from './CommentDetail'
 
 const App = () => {
    return (
-    <div class="ui comments">
-        <div class="comment">
-        <a class="avatar">
-            <img src={faker.image.avatar()}/>
-        </a>
-            <div class="content">
-                <a class="author">Stevie Feliciano</a>
-                <div class="metadata">
-                <div class="date">2 days ago</div>
-                <div class="rating">
-                    <i class="star icon"></i>
-                    5 Faves
-                </div>
-                </div>
-                <div class="text">
-                Hey guys, I hope this example comment is helping you read this documentation.
-                </div>
-            </div>
-        </div>
-
-        <div class="comment">
-        <a class="avatar">
-            <img src={faker.image.avatar()}/>
-        </a>
-            <div class="content">
-                <a class="author">Stevie Feliciano</a>
-                <div class="metadata">
-                <div class="date">2 days ago</div>
-                <div class="rating">
-                    <i class="star icon"></i>
-                    5 Faves
-                </div>
-                </div>
-                <div class="text">
-                Hey guys, I hope this example comment is helping you read this documentation.
-                </div>
-            </div>
-        </div>
+    <div class="ui container comments">
+        <CommentDetail />
+        <CommentDetail />
+        <CommentDetail />
     </div>
    )
 }
